e34f4d25a720822f945383e539f9d3d124b061e9
jeewon <tnstlasla@gmail.com>
2021-08-18 21:11:09 +0900

    remove result

--- 이지원2652/src/a7_조건/Quiz2753.java
+++ 이지원2652/src/a7_조건/Quiz2753.java
@@ -7,14 +7,11 @@ public class Quiz2753 {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		
-		int year = input.nextInt();
-		int result;
+		int year = input.nextInt();		
 				
 		if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ) {
-			result = 1;
 			System.out.println("¿±≥‚¿‘¥œ¥Ÿ.!!");
 		} else {
-			result = 0;
 			System.out.println("¿±≥‚¿Ã æ∆¥’¥œ¥Ÿ.!!");
 		}
 		
