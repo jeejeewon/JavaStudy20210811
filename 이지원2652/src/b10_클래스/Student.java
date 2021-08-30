45478e17c21f018397025fe8a5081dfe143a9495
jeewon <tnstlasla@gmail.com>
2021-08-30 21:54:57 +0900

    2021/08/30

--- 이지원2652/src/b10_클래스/Student.java
+++ 이지원2652/src/b10_클래스/Student.java
@@ -1,20 +1,64 @@
-package b10_클래스;
+ package b10_클래스;
 
 public class Student {
 	
-	public Student() {
+	private String name;
+	private String schoolName;
+	private int year;	
+	// 모든 변수는 캡슐화
+	
+	Student() { 
+		// 기본 생성자
 		// 반환형이 없음
 		// 메모리주소
 	}
-	
-	String name;
-	String schoolName;
-	String year;
-	
+	public Student(String name, int year) {
+		super(); // 상속 관계
+		this.name = name;	
+		this.year = year;
+		this.showInfo();	
+	}			
+	public Student(String name, String schoolName, int year) {
+		super(); // 상속 관계
+		this.name = name;
+		this.schoolName = schoolName;
+		this.year = year;
+		this.showInfo();	
+	}	
+	public String getName( ) {
+		return name;
+	}		
+	public void setName(String name) {
+		this.name = name + " 학생";		
+	}	
+	public String getSchoolName() {
+		return schoolName;
+	}
+	public void setSchoolName(String schoolName) {
+		this.schoolName = schoolName;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}	
+	public void getFreebies() {
+		Regular regular = new Regular();
+		
+		regular.setYearCheck(year, schoolName);
+				
+		if(regular.isYearCheck()) {
+			System.out.println("사은품 대상자");			
+		} else {
+			System.out.println("대상자 제외");
+		}		
+	}	
 	public void showInfo() {
 		System.out.println("학생 정보");
 		System.out.println("이름: " + name);
 		System.out.println("학교: " + schoolName);
 		System.out.println("학년: " + year);
+	
 	}
 }
--- 이지원2652/src/b10_클래스/StudentTest.java
+++ 이지원2652/src/b10_클래스/StudentTest.java
@@ -2,16 +2,24 @@ package b10_클래스;
 
 public class StudentTest {
 	public static void main(String[] args) {
-		Student kim = new Student(); // 생성자 -> 클래스명과 같음
-		kim.name = "김재성";
-		kim.showInfo();		
+		Student lee = new Student("이지원", 2);
+		Student kim = new Student("김준일", "USA아이티", 4);
+		Student park = new Student("박호훈","코리아아이티", 3);
 		
-		Student lee = new Student();
-		lee.name = "이효원";
-		lee.showInfo();
 		
-		System.out.println(kim); 
-		System.out.println(lee);
+		// kim.name = "김준일"; // .은 해당 '메모리 주소값을 참조한다'라고 함 
+				
+		lee.showInfo();		
+		
+		Integer i = new Integer(100);
+		
+		System.out.println(lee.hashCode()); // heap메모리주소
+		System.out.println(i.hashCode());
+		
+		lee.getFreebies();
+		kim.getFreebies();
+		park.getFreebies();
+		
 	}
 
 }
