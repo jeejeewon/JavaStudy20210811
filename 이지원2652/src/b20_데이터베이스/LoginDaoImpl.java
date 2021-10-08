2d53c498dca437580b87522207067475049f1eea
이지원 <tnstlasla@gmail.com>
2021-10-08 17:27:30 +0900

    2021/10/08

--- 이지원2652/src/b20_데이터베이스/LoginDaoImpl.java
+++ 이지원2652/src/b20_데이터베이스/LoginDaoImpl.java
@@ -15,8 +15,42 @@ public class LoginDaoImpl implements LoginDao {
 		pool = DBConnectionMgr.getInstance();
 	}
 	
-	// 로그인 로직	
+	
+	// 로그인 로직		
 	@Override
+	public int login(String id, String password) {
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = null;
+		int flag = 0;
+		
+		try {
+			con = pool.getConnection();
+			sql = "select count(um.user_id), count(ud.user_password) from "
+					+ "user_mst um "
+					+ "left outer join user_mst ud on (ud.user_id = um.user_id and ud.user_password = ?)"
+					+ "where um.user_id = ?";	
+			pstmt = con.prepareStatement(sql);
+			pstmt.setString(1, password);
+			pstmt.setString(2, id);			
+			rs = pstmt.executeQuery();
+
+			rs.next();
+			flag = rs.getInt(1) + rs.getInt(2);
+
+		} catch (Exception e) {			
+			e.printStackTrace();
+		} finally {			
+			pool.freeConnection(con, pstmt, rs);
+		}
+
+		return flag;
+
+	}
+	
+	
+	/*@Override
 	public int login(String id, String password) {
 		Connection con = null;
 		PreparedStatement pstmt = null;
@@ -50,7 +84,7 @@ public class LoginDaoImpl implements LoginDao {
 			pool.freeConnection(con, pstmt, rs);
 		}		
 		return flag;
-	}
+	}*/
 
 	@Override
 	public String getLoginUserName(String id) {
@@ -78,5 +112,6 @@ public class LoginDaoImpl implements LoginDao {
 		return name;
 		
 	}
+	
 
 }
